93b09800805152dbf9828199261cde92b776e1b4
sansRebel <khaledalsinafi101@gmail.com>
2025-06-18 14:00:50 +0800

    changed authentication route

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json()); 
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
+app.use("/api/auth/", authRoutes);
 app.use("/api/protected", protectedRoutes);
 
 const PORT = process.env.PORT || 5000;
